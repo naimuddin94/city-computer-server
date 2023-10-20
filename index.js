062219c825c5fc37883608bd1abdb81254228cd7
naimuddin94 <developernaimuddin@gmail.com>
2023-10-20 20:45:26 +0600

    some bug fix

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function run() {
     // delete product from cart database
     app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
+      const query = { _id: id };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
