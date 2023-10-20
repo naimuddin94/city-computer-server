af97ca832d9cca06f75f8398d5e09378bbee2aaa
naimuddin94 <developernaimuddin@gmail.com>
2023-10-20 20:06:19 +0600

    saved cart item with unique

--- index.js
+++ index.js
@@ -57,9 +57,10 @@ async function run() {
       res.send(result);
     });
 
+    // delete product from cart database
     app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: id };
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
